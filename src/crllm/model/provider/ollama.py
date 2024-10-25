96f37e56993452ec24503b5376d9ca2b3237ae2b
Lukas Rump <lukasrump.dev@gmail.com>
2024-10-25 17:24:27 +0200

    feat: add base url envvar for ollama

--- src/crllm/model/provider/ollama.py
+++ src/crllm/model/provider/ollama.py
@@ -1,9 +1,13 @@
+import os
 from langchain_ollama import ChatOllama
 from crllm.model.model import Model
 
 
 class OllamaModel(Model):
     def _get_model(self, model_config):
+        if os.environ.get("OLLAMA_URL"):
+            model_config["base_url"] = os.environ.get("OLLAMA_URL")
+
         return ChatOllama(**model_config)
 
     @staticmethod
